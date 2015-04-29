16c432b0dad3df283bbeb10ad08e10da7855f5f7
juanjooo172 <juanjooo172@gmail.com>
2015-04-29 09:59:18 +0200

    1.1
    Signed-off-by: juanjooo172 <juanjooo172@gmail.com>

---
+++ ZayaSoft/src/MODULO1.java
@@ -0,0 +1,9 @@
+
+public class MODULO1 {
+	// ejercicio
+	void imprimeN(int n){
+		for(int i=0;i<=n;i++){
+			System.out.println("Modulo ejecutandose");
+		}
+	}
+}
---
+++ ZayaSoft/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		MODULO1 M = new MODULO1();
+		M.imprimeN(10);
+	}
+
+}
