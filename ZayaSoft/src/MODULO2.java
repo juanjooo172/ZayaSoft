b368be82950f86deb5e0d145ef311cb4424cc970
juanjooo172 <juanjooo172@gmail.com>
2015-04-30 09:01:47 +0200

    1.2

---
+++ ZayaSoft/src/MODULO2.java
@@ -0,0 +1,29 @@
+
+public class MODULO2 {
+
+	public int suma(int operando1, int operando2)
+	{
+		int resultado = operando1 + operando2;
+		return  resultado;
+		
+	}
+	public int resta(int operando1, int operando2)
+	{
+		int resultado = operando1 - operando2;
+		return  resultado;
+		
+	}
+	public int multiplica(int operando1, int operando2)
+	{
+		int resultado = operando1 * operando2;
+		return  resultado;
+		
+	}
+	public int divide(int operando1, int operando2)
+	{
+		int resultado = operando1 / operando2;
+		return  resultado;
+		
+	}
+	
+}
--- ZayaSoft/src/Main.java
+++ ZayaSoft/src/Main.java
@@ -3,8 +3,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		MODULO1 M = new MODULO1();
+		/*MODULO1 M = new MODULO1();
 		M.imprimeN(10);
+		MODULO2 M2 = new MODULO2();
+		System.out.println("La Suma : "+M2.suma(5, 7));
+		System.out.println("La resta : "+M2.resta(7, 5));
+		System.out.println("La Multiplucacion : "+M2.multiplica(5, 7));
+		System.out.println("La Division : "+M2.divide(10, 5));*/
+		
+		
 	}
 
 }
