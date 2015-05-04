757e9d28b83b2d6d3c16059c4afbeacc20488c77
juanjooo172 <juanjooo172@gmail.com>
2015-05-04 12:53:15 +0200

    1.6
    Signed-off-by: juanjooo172 <juanjooo172@gmail.com>

--- ZayaSoft/src/Main.java
+++ ZayaSoft/src/Main.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+	
 		/*MODULO1 M = new MODULO1();
 		M.imprimeN(10);
 		MODULO2 M2 = new MODULO2();
@@ -10,8 +12,33 @@ public class Main {
 		System.out.println("La resta : "+M2.resta(7, 5));
 		System.out.println("La Multiplucacion : "+M2.multiplica(5, 7));
 		System.out.println("La Division : "+M2.divide(10, 5));*/
+		MODULO2 M2 = new MODULO2();
+		Scanner numero= new Scanner(System.in);
+		int opcion = numero.nextInt();
 		
 		
+		System.out.println("primer numero");
+		int x =numero.nextInt();
+		
+		System.out.println("segundo numero");
+		int y =numero.nextInt();
+		
+		switch(opcion)
+	    {
+	        case 1:
+	            System.out.println(M2.suma(x, y));
+	            break;
+	        case 2:
+	        	System.out.println(M2.resta(x, y));
+	            break;
+	        case 3:
+	        	System.out.println(M2.multiplica(x, y));
+	            break;
+	        case 4:
+	        	System.out.println(M2.divide(x, y));
+	           break;
 	}
+		
 
+}
 }
